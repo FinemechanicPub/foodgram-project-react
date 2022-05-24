bc220431b857ccc9b10d6a85a50ab66aca3ccb2d
Alexander Rubtsov <equester@mail.ru>
2022-05-24 12:33:07 +0300

    Add WebUser admin

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import WebUser
 
-# Register your models here.
+
+@admin.register(WebUser)
+class WebUserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'email', 'is_staff')
+    list_display_links = ('username',)
+    search_fields = ('email', 'username')
+    list_filter = ('email', 'username')
