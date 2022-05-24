2df58d0f2369c275376391c0abeb0e1b10e95005
Alexander Rubtsov <equester@mail.ru>
2022-05-25 00:47:38 +0300

    Add command for loading sample data

---
+++ backend/foodgram/recipes/management/commands/sampledata.py
@@ -0,0 +1,80 @@
+"""Модуль загрузки тестовых данных
+
+Файлы csv должны в первой строке иметь названия полей модели.
+Если поле является внешним ключом, то возможны два варианта:
+- поле называется по шаблону {название поля}_id и содержит id в таблице
+  внешнего ключа
+- поле называется по шаблону {название поля} и содержит значение соответ-
+  ствующего поля в таблице внешнего ключа
+Во втором случае в словаре MODELS вторым компонентом значения должен быть
+словарь, в котором сопоставлено имя поля модели и имя поля в модели внешнего
+ключа.
+Словарь MODELS содержит в качестве ключей загружаемые модели, а в качестве
+занчений кортеж из имени файла .csv без расширения и словаря внешних ключей
+(смотри выше).
+
+"""
+import csv
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from django.db.models import Model
+from recipes.models import Ingredient
+
+CSV_DIR = os.path.join(settings.BASE_DIR, 'data/')
+
+
+class Command(BaseCommand):
+    help = f'Loads sample data from CSV files in "{CSV_DIR}"'
+
+    def load_csv(self, model: Model, filename: str, related={}):
+        related_models = {
+            field_name: (
+                getattr(model, field_name).field.related_model,
+                related_field_name
+            )
+            for field_name, related_field_name in related.items()
+        }
+        with open(
+            os.path.join(CSV_DIR, filename),
+            mode='r', encoding='utf-8', newline=''
+        ) as file:
+            reader = csv.reader(file)
+            headers = next(reader)
+            for row in reader:                
+                params = dict(zip(headers, row))
+                if hasattr(model.objects, 'create_user'):                    
+                    model.objects.create_user(**params)
+                    continue
+                instance = model()
+                for key, value in params.items():
+                    key_id = f'{key}_id'
+                    if hasattr(instance, key_id):
+                        if key in related_models:
+                            related_model, related_field_name = (
+                                related_models[key]
+                            )
+                            related_object, _ = (
+                                related_model.objects
+                                .get_or_create(**{related_field_name: value})
+                                )
+                            setattr(instance, key, related_object)
+                        else:
+                            setattr(instance, key_id, value)
+                    else:
+                        setattr(instance, key, value)
+                instance.save()
+
+    MODELS = {
+        Ingredient: ('ingredients', {'units': 'notation'})
+    }
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        try:
+            for model, (filebase, related) in self.MODELS.items():
+                self.load_csv(model, f'{filebase}.csv', related)
+        except Exception as error:
+            raise CommandError(f'что-то пошло не так. {error}')
