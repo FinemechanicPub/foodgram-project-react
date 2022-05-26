bf58e47309d7ccb35e281094ff8a29eb4ec7433a
Alexander Rubtsov <equester@mail.ru>
2022-05-26 19:23:27 +0300

    Setup custom user model

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .validators import username_validator
+
 
 class WebUser(AbstractUser):
-    pass
+    username = models.CharField(
+        'имя пользователя',
+        max_length=150,
+        unique=True,
+        validators=(username_validator(),)
+    )
+    email = models.EmailField('электронная почта', max_length=254, unique=True)
+    first_name = models.CharField(
+        'имя',
+        max_length=150,
+        null=True, blank=False
+    )
+    last_name = models.CharField(
+        'фамилия',
+        max_length=150,
+        null=True, blank=False
+    )
+
+
