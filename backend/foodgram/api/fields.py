7ec91d26e2d14828577493e637bc2cbe57453df0
Alexander Rubtsov <equester@mail.ru>
2022-05-28 11:32:44 +0300

    Fix bugs

---
+++ backend/foodgram/api/fields.py
@@ -0,0 +1,34 @@
+"""Дополнительные поля для сериализаторов"""
+import base64
+from distutils.log import error
+
+from django.core.files.base import ContentFile
+from rest_framework import serializers
+
+
+class DecodingImageField(serializers.ImageField):
+    """Поле изображение с декодированием из Base64"""
+
+    BASE64_SIGNATURE = ';base64,'
+
+    def to_internal_value(self, data):
+        if self.BASE64_SIGNATURE not in data:
+            raise serializers.ValidationError(
+                f'В данных не обнаружено "{self.BASE64_SIGNATURE}"'
+            )
+        headers, text = data.split(self.BASE64_SIGNATURE, 1)
+        if '/' not in headers:
+            raise serializers.ValidationError('Неожиданный формат запроса.')
+        content_type, image_type = headers.split('/', 1)
+        if content_type != 'data:image':
+            raise serializers.ValidationError('Неожиданный формат запроса.')
+        try:
+            data = ContentFile(
+                base64.b64decode(text),
+                f'uploaded_file.{image_type}'
+            )
+        except Exception as error:
+            raise serializers.ValidationError(
+                f'Некорректный формат файла: {error}'
+            )
+        return super().to_internal_value(data)
