8ce9449484bba5995e1198c2a7fa1506822d5772
Alexander Rubtsov <equester@mail.ru>
2022-05-29 19:50:31 +0300

    Add create and update to the Recipe serializer; fix issues

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -34,13 +34,9 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 class RecipeIngredientSerializer(serializers.ModelSerializer):
 
-
-    #ingredient = IngredientSerializer()
-
     class Meta:
         model = RecipeIngredient
-        fields = ('ingredient', 'amount', 'ingredient_id')
-        read_only_fields = ('ingredient',)
+        fields = ('ingredient', 'amount')
     
     # При отображении запрашивать вложенный сериализатор
     # и дополнять ответ его данными без вложенности
@@ -48,6 +44,11 @@ class RecipeIngredientSerializer(serializers.ModelSerializer):
         representation = IngredientSerializer(instance.ingredient).data
         representation.update({'amount': instance.amount})
         return representation
+    
+    # Приведение данных к форме модели
+    def to_internal_value(self, data):
+        data['ingredient'] =  data.pop('id')
+        return super().to_internal_value(data)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -56,32 +57,40 @@ class RecipeSerializer(serializers.ModelSerializer):
         read_only=True,
         default=serializers.CurrentUserDefault()
     )
+    image = DecodingImageField()
     ingredients = RecipeIngredientSerializer(
         many=True,
-        source='recipe_to_ingredients'
+        source = 'recipe_to_ingredients'
     )
-    image = DecodingImageField()
 
     class Meta:
         model = Recipe
         fields = (
-            'id', 'tags', 'author', 'ingredients',
-            'name', 'image', 'text', 'cooking_time'
+            'id', 'tags', 'author',
+            'ingredients', 'name', 'image', 'text', 'cooking_time'
         )
     
-    # При отображении запрашивать вложенный сериализатор
+    # При отображении запрашивать вложенный сериализатор тегов
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation['tags'] = TagSerializer(instance.tags, many=True).data
         return representation
     
     def create(self, validated_data):
-        print('Validated_data: ', validated_data)
-        ingredients = validated_data.pop('recipe_to_ingredients')
-        tags = validated_data.pop('tags')
-        recipe = Recipe.objects.create(**validated_data)
-        recipe.tags.set(tags)
-        for ingredient in ingredients:
-            RecipeIngredient.objects.create(recipe=recipe, **ingredient)
+        # print('Validated_data: ', validated_data)
+        recipe_items = validated_data.pop('recipe_to_ingredients')
+        recipe = super().create(validated_data)
+        for item in recipe_items:
+            recipe.recipe_to_ingredients.create(**item)
         return recipe
+    
+    def update(self, instance, validated_data):
+        # print('Validated_data (update): ', validated_data)
+        recipe_items = validated_data.pop('recipe_to_ingredients')
+        instance = super().update(instance, validated_data)
+        instance.recipe_to_ingredients.all().delete()
+        for item in recipe_items:
+            instance.recipe_to_ingredients.create(**item)
+        instance.save()
+        return instance
         
