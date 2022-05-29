acea32f47390374a4926f4074165a76751195b8a
Alexander Rubtsov <equester@mail.ru>
2022-05-30 00:16:36 +0300

    Add filter by tags

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,5 +1,6 @@
+from django import forms
 from django_filters import rest_framework as filter
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe, Tag
 
 
 class IngredientFilter(filter.FilterSet):
@@ -9,8 +10,18 @@ class IngredientFilter(filter.FilterSet):
         Model = Ingredient
 
 
+class MultipleCharFilter(filter.MultipleChoiceFilter):
+    field_class = forms.CharField
+
+
 class RecipeFilter(filter.FilterSet):
     author = filter.NumberFilter(lookup_expr='exact', field_name='author__id')
+    tags = filter.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
+        to_field_name='slug',
+        lookup_expr='exact',
+        field_name='tags__slug'
+    )
 
     class Meta:
         Model = Recipe
