bf98c519759d6ffa0d6a4d53c47c6c3cd5b6255c
Alexander Rubtsov <equester@mail.ru>
2022-05-29 19:54:49 +0300

    Formatting

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -8,20 +8,20 @@ from .serializers import IngredientSerializer, RecipeSerializer, TagSerializer
 
 
 class TagViewset(viewsets.ReadOnlyModelViewSet):
-    serializer_class = TagSerializer
     queryset = Tag.objects.all()
+    serializer_class = TagSerializer
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = IngredientFilter
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
     pagination_class = RecipePagination
 
     def perform_create(self, serializer):
