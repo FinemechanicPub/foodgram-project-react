e933b09121398b1d6036b7207ccda7f8754ad0c4
Alexander Rubtsov <equester@mail.ru>
2022-05-30 09:28:43 +0300

    Add query logging

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -155,3 +155,20 @@ DJOSER = {
 RECIPES = {
     'TEXT_DISPLAY_LENGTH': 30
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console']
+        }
+    }
+}
