f09cf76fd620270de7bc0740476a2227ec9190a8
Alexander Rubtsov <equester@mail.ru>
2022-06-17 10:26:12 +0300

    Formatting

--- backend/foodgram/api/services.py
+++ backend/foodgram/api/services.py
@@ -9,16 +9,16 @@ LIST = """
 
 
 def render_txt(shopping_list: list, filename: str):
-        ingredient_text = '\n'.join(
-            LIST_ITEM.format(
-                name =item['ingredient_name'],
-                unit=item['unit'],
-                amount=item['total_amount']
-            ) for item in shopping_list
-        )
-        return ContentFile(
-            LIST.format(
-                ingredients=ingredient_text
-            ).encode(encoding='utf-8'),
-            f'{filename}.txt'
-        )
+    ingredient_text = '\n'.join(
+        LIST_ITEM.format(
+            name=item['ingredient_name'],
+            unit=item['unit'],
+            amount=item['total_amount']
+        ) for item in shopping_list
+    )
+    return ContentFile(
+        LIST.format(
+            ingredients=ingredient_text
+        ).encode(encoding='utf-8'),
+        f'{filename}.txt'
+    )
