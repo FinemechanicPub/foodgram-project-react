c1442b4264d5a978aa0ac64c7303a872da53fcc8
Alexander Rubtsov <equester@mail.ru>
2022-06-17 12:07:11 +0300

    Formatting

--- backend/foodgram/api/pagination.py
+++ backend/foodgram/api/pagination.py
@@ -1,6 +1,7 @@
 """"Модуль специализированных классов пагинации"""
 from rest_framework.pagination import PageNumberPagination
 
+
 class RecipePagination(PageNumberPagination):
     page_size_query_param = 'limit'
     page_size = 5
